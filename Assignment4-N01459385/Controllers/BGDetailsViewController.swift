018632da3ae7d05b8f763e572d6639dc159fb1cf
LFClaro <lfclaro@gmail.com>
2021-12-15 02:16:27 -0300

    final commit

--- Assignment4-N01459385/Controllers/BGDetailsViewController.swift
+++ Assignment4-N01459385/Controllers/BGDetailsViewController.swift
@@ -102,16 +102,5 @@ class BGDetailsViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Assignment4-N01459385/Controllers/myCollectionViewController.swift
+++ Assignment4-N01459385/Controllers/myCollectionViewController.swift
@@ -18,27 +18,7 @@ class myCollectionViewController: UICollectionViewController {
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-pattern.jpeg")!)
         allBoardGames = CoreDataService.shared.getAllBoardGames()
         collectionView.reloadData()
-        
-//        (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.estimatedItemSize = CGSize(width: 50, height: 50)
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -123,32 +103,4 @@ class myCollectionViewController: UICollectionViewController {
         alert.addAction(ok)
         self.present(alert, animated: true, completion: nil)
     }
-    
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-    */
-
 }
